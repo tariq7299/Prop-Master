90342259e8d76936d56c07cc2262465e73449254
tariq7299 <tr.sar77an78@gmail.com>
2024-10-01 17:38:13 +0300

    :

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -54,8 +54,7 @@ export default function Sidebar({
             <div
               className={`flex flex-col justify-end truncate ${isCollapsed ? 'invisible w-0' : 'visible w-auto'}`}
             >
-              <span className='font-medium'>Shadcn Admin</span>
-              <span className='text-xs'>Vite + ShadcnUI</span>
+              <span className='font-medium'>Prop Master</span>
             </div>
           </div>
 
